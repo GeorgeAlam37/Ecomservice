64dcd1ac067c3cd79dc3f5733a074aa78452a867
George Alam <georgealam37@gmail.com>
2024-06-16 05:09:12 +0200

    george

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ const whenExternalScripts = (items = []) =>
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  output: 'static',
+  output: 'dist',
   site: 'https://georgealam37.github.io/ecomservice/',
   base: '/ecomservice/',
   integrations: [
